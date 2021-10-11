d4279a0b48f6b80d7dbb6c4320b4b59f5835c237
satyam patel <92255366+satyampatel13@users.noreply.github.com>
2021-10-11 09:55:17 +0530

    Create q8.py

---
+++ q8.py
@@ -0,0 +1,10 @@
+a = int(input("enter first number : "))
+b = int(input("enter first number : "))
+c = int(input("enter first number : "))
+d = int(input("enter first number : "))
+
+numberList = [a, b,c, d]
+
+total = numberList[0] + numberList[1] + numberList[2] + numberList[3]
+
+print(total)
