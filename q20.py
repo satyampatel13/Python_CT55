e09a22a056ee87c5334173a78fdb7bee56565784
satyam patel <92255366+satyampatel13@users.noreply.github.com>
2021-10-11 10:26:00 +0530

    Create q20.py

---
+++ q20.py
@@ -0,0 +1,13 @@
+m1 = int(input("Marks of student : "))
+if(m1 > 90 and x <= 100):
+    print("Excellent")
+elif(m1 > 80 and x <=90):
+    print("A")
+elif(m1 > 70 and x <= 80):
+    print("B")
+elif(m1 > 60 and x <= 50):
+    print("C")
+elif(m1 > 50 and x <= 60):
+    print("D")
+else:
+    print("fail")
